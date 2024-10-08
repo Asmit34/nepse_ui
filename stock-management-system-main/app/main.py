08777d422f5b347921723ad7eb4b3b781faf8011
Asmit oli <82573564+Asmit34@users.noreply.github.com>
2024-10-08 15:25:33 +0545

    Update main.py

--- stock-management-system-main/app/main.py
+++ stock-management-system-main/app/main.py
@@ -10,18 +10,14 @@ db_config = {
     'database': 'nepse'
 }
 
-# Create the database connection using SQLAlchemy
 connection_string = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
 engine = create_engine(connection_string)
 
-# Read CSV file into pandas DataFrame
 csv_file_path = r'C:\Users\Asmit\Desktop\stock-management-system-main\agm_reports.csv'
 df = pd.read_csv(csv_file_path)
 
-# Define the table name where you want to load the data
 table_name = 'agm_reports'
 
-# Load DataFrame to PostgreSQL table
 df.to_sql(table_name, engine, if_exists='append', index=False)
 
 print(f"Data from {csv_file_path} has been successfully loaded into {table_name} table.")
